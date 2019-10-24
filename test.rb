1eb946b332455f280b6078e2097b12e290fda8b0
Aheebwa Ramadhan <raheebwa@hotmail.com>
2019-10-24 15:09:13 +0300

    Added testcases for arguments in #my_all?

--- test.rb
+++ test.rb
@@ -19,8 +19,13 @@ def solve
   # p numbers
 
   # TEST FOR #my_all?
-  # numbers = [1, 2, 3, 4, 5]
-  # p numbers.my_all? { |x| x < 6 }
+  p %w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
+  p %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
+  p %w[ant bear cat].my_all?(/t/)                        #=> false
+  p [1, 2i, 3.14].my_all?(Numeric)                       #=> true
+  p [nil, true, 99].my_all?                              #=> false
+  p [].my_all?                                           #=> true
+
 
   # TEST FOR #my_any?
   # numbers = [1, 2, 3, 4, 5, 6]
